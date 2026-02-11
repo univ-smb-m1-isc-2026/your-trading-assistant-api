d8551e4bda7c997c971e1ce2ae451fce0c6dd8f3
Evan <evan.email@hidden.local>
2026-02-11 11:05:01 +0100

    init springboot project

---
+++ src/main/java/fr/info803/trading_assistant/TradingAssistantApplication.java
@@ -0,0 +1,13 @@
+package fr.info803.trading_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/info803/trading_assistant/TradingAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.info803.trading_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
